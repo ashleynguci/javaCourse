dd898effb2c98b7c967294a6cb367f0731a14865
ashleynguci <40316179+ashleynguci@users.noreply.github.com>
2019-02-18 22:20:33 +0200

    Array exercise

---
+++ ArrayWarmUp.java
@@ -0,0 +1,12 @@
+
+public class ArrayWarmUp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] Array = new int[] { 1, 5, 2, 6, 7 };
+		for (int i = 0; i < Array.length; i++) {
+			System.out.print(Array[i] + " ");
+		}
+	}
+
+}
