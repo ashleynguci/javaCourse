6953bb931d41f14e08350d9513891475d95177a0
ashleynguci <40316179+ashleynguci@users.noreply.github.com>
2019-02-20 22:43:40 +0200

    Object Class program

---
+++ ObjectsPizzaProgram.java
@@ -0,0 +1,13 @@
+
+public class ObjectsPizzaProgram {
+
+	public static void main(String[] args) {
+		Pizza pizzaOne = new Pizza("Diavolo",8.75);
+		Pizza pizzaTwo = new Pizza("Frutti al Mare",9.00);
+		Pizza pizzaThree = new Pizza("Alla Pollo",7.50);
+		System.out.println(pizzaOne.toString());
+		System.out.println(pizzaTwo.toString());
+		System.out.println(pizzaThree.toString());
+	}
+
+}
